93d3313e4cb824ec3bcac67504711386f1e650ba
Aleksey Tretyak <aleksey.tretyak@gmail.com>
2016-04-18 20:33:32 +0300

    build digraph

--- src/WordNet.java
+++ src/WordNet.java
@@ -1,8 +1,34 @@
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+
 public class WordNet {
+    private Digraph digraph;
+    private ArrayList<String[]> s = new ArrayList<>();
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
+        In inS = new In(synsets);
+        In inH= new In(hypernyms);
 
+        while (inS.hasNextLine()) {
+            String[] lineData = inS.readLine().split(",");
+            s.add(new String[]{lineData[1], lineData[2]});
+        }
+
+        digraph = new Digraph(s.size());
+
+        while (inH.hasNextLine()) {
+            String[] lineData = inH.readLine().split(",");
+            for (int i = 1; i < lineData.length; i++) {
+                digraph.addEdge(Integer.parseInt(lineData[0]), Integer.parseInt(lineData[i]));
+            }
+        }
+        System.out.println(digraph.E());
     }
 
     // returns all WordNet nouns
@@ -28,6 +54,7 @@ public class WordNet {
 
     // do unit testing of this class
     public static void main(String[] args) {
-
+        WordNet wordNet = new WordNet("./wordnet/synsets.txt", "./wordnet/hypernyms.txt");
+        In in = new In("./wordnet/hypernyms.txt");
     }
 }
