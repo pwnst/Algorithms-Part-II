4d4c9bc151652910694587ddbd5bf6d98327fd18
Aleksey Tretyak <aleksey.tretyak@gmail.com>
2016-04-20 08:37:17 +0300

    fix

--- src/SAP.java
+++ src/SAP.java
@@ -1,20 +1,86 @@
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.LinkedQueue;
 
 public class SAP {
+    private Digraph digraph;
 
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G) {
-
+        digraph = G;
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-        return 1;
+        int[] path = findPath(v, w);
+        if (path == null) {
+            return -1;
+        }
+        int count = 1;
+        int cur = path[w];
+        while (cur != v) {
+            cur = path[cur];
+            count++;
+        }
+        return count;
+    }
+
+    private int[] findPath(int v, int w) {
+        int[] path = new int[digraph.V()];
+        boolean[] visited = new boolean[digraph.V()];
+
+        LinkedQueue<Integer> queue = new LinkedQueue<>();
+        queue.enqueue(v);
+
+        while (!queue.isEmpty()) {
+            int current = queue.dequeue();
+            visited[current] = true;
+            if (current == w) {
+                return path;
+            }
+            Iterable<Integer> adj = digraph.adj(current);
+            for (Integer a : adj) {
+                if (!visited[a]) {
+                    queue.enqueue(a);
+                    path[a] = current;
+                }
+            }
+        }
+        return null;
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w) {
-        return 1;
+        boolean[] visited = new boolean[digraph.V()];
+        LinkedQueue<Integer> queue = new LinkedQueue<>();
+        int common = -1;
+
+        queue.enqueue(v);
+        while (!queue.isEmpty()) {
+            int current = queue.dequeue();
+            visited[current] = true;
+            Iterable<Integer> adj = digraph.adj(current);
+            for (Integer a : adj) {
+                if (!visited[a]) {
+                    queue.enqueue(a);
+                }
+            }
+        }
+
+        queue.enqueue(w);
+        while (!queue.isEmpty()) {
+            int current = queue.dequeue();
+            if (visited[current]) {
+                common = current;
+                break;
+            }
+            Iterable<Integer> adj = digraph.adj(current);
+            for (Integer a : adj) {
+                queue.enqueue(a);
+            }
+        }
+
+        return common;
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
@@ -29,6 +95,8 @@ public class SAP {
 
     // do unit testing of this class
     public static void main(String[] args) {
-
+        Digraph d = new Digraph(new In("./wordnet/digraph1.txt"));
+        SAP sap = new SAP(d);
+        System.out.println(sap.ancestor(4, 8));
     }
 }
--- src/WordNet.java
+++ src/WordNet.java
@@ -3,13 +3,14 @@ import edu.princeton.cs.algs4.In;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.HashMap;
 
 
 public class WordNet {
     private Digraph digraph;
-    private ArrayList<ArrayList<String>> s = new ArrayList<>();
-    private HashSet<String> nouns = new HashSet<>();
+    private ArrayList<ArrayList<String>> synArray = new ArrayList<>();
+    private HashMap<String, Integer> nouns = new HashMap<>();
+    private SAP sap;
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
@@ -19,13 +20,15 @@ public class WordNet {
         while (inS.hasNextLine()) {
             String[] lineData = inS.readLine().split(",");
             String[] synset = lineData[1].split(" ");
-            nouns.addAll(Arrays.asList(synset));
+            for (String s : synset) {
+                nouns.put(s, Integer.parseInt(lineData[0]));
+            }
             ArrayList<String> data = new ArrayList<>(Arrays.asList(synset));
             data.add(lineData[2]);
-            s.add(data);
+            synArray.add(data);
         }
 
-        digraph = new Digraph(s.size());
+        digraph = new Digraph(synArray.size());
 
         while (inH.hasNextLine()) {
             String[] lineData = inH.readLine().split(",");
@@ -33,31 +36,39 @@ public class WordNet {
                 digraph.addEdge(Integer.parseInt(lineData[0]), Integer.parseInt(lineData[i]));
             }
         }
+
+        sap = new SAP(digraph);
     }
 
     // returns all WordNet nouns
     public Iterable<String> nouns() {
-        return nouns;
+        return nouns.keySet();
     }
 
     // is the word a WordNet noun?
     public boolean isNoun(String word) {
-        return nouns.contains(word);
+        return nouns.containsKey(word);
     }
 
     // distance between nounA and nounB (defined below)
     public int distance(String nounA, String nounB) {
-        return 0;
+        int nA = nouns.get(nounA);
+        int nB = nouns.get(nounB);
+        return sap.length(nA, nB);
     }
 
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB) {
-        return null;
+        int nA = nouns.get(nounA);
+        int nB = nouns.get(nounB);
+        int ancestor = sap.ancestor(nA, nB);
+        return synArray.get(ancestor).toString();
     }
 
     // do unit testing of this class
     public static void main(String[] args) {
         WordNet wordNet = new WordNet("./wordnet/synsets.txt", "./wordnet/hypernyms.txt");
+        System.out.println(wordNet.sap("bird", "worm"));
     }
 }
